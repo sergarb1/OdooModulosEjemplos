9754f69dde17516df398533194458c8ab40fbd51
sergarb1 <sergi.profesor@gmail.com>
2021-11-03 11:19:40 +0100

    Primeros modulos

---
+++ EJ02-ListaTareas/models/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+#Definimos el modelo de datos
+class lista_tareas(models.Model):
+    #Nombre y descripcion del modelo de datos
+    _name = 'lista_tareas.lista'
+    _description = 'Modelo de la lista de tareas'
+
+    #Elementos de cada fila del modelo de datos
+    #Los tipos de datos a usar en el ORM son 
+    # https://www.odoo.com/documentation/14.0/developer/reference/addons/orm.html#fields
+   
+    tarea = fields.Char()
+    prioridad = fields.Integer()
+    urgente = fields.Boolean(compute="_value_urgente", store=True)
+    realizada = fields.Boolean()
+
+
+    #Este computo depende de la variable prioridad
+    @api.depends('prioridad')
+    #Funcion para calcular el valor de urgente
+    def _value_urgente(self):
+        #Para cada registro
+        for record in self:
+            #Si la prioridad es mayor que 10, se considera urgente, en otro caso no lo es
+            if record.prioridad>10:
+                record.urgente = True
+            else:
+                record.urgente = False
